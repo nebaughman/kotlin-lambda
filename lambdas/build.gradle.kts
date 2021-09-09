a5a2d8dd2fabb407896f4a036ebd1793a90193af
Nathaniel Baughman <neb@nebaughman.com>
2021-09-09 17:14:06 -0400

    Configure gradle dependencies

--- lambdas/build.gradle.kts
+++ lambdas/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     id("kotlin.lambda.kotlin-application-conventions")
+    kotlin("plugin.serialization") version "1.5.20"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 dependencies {
+    implementation("com.amazonaws:aws-lambda-java-events:3.9.0")
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
 }
 
 application {
--- stack/build.gradle.kts
+++ stack/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     id("kotlin.lambda.kotlin-library-conventions")
 }
+
+dependencies {
+    val cdkVersion = "1.115.0"
+    implementation("software.amazon.awscdk:core:${cdkVersion}")
+    implementation("software.amazon.awscdk:lambda:${cdkVersion}")
+    implementation("software.amazon.awscdk:apigateway:${cdkVersion}")
+}
