e9faab23f037b5377746dbd8509976fb342cd31d
Nathaniel Baughman <neb@nebaughman.com>
2021-09-11 10:12:45 -0400

    Add ApiGateway endpoint with Lambda integration

--- lambdas/src/main/kotlin/example/lambda/LambdaHandler.kt
+++ lambdas/src/main/kotlin/example/lambda/LambdaHandler.kt
@@ -2,16 +2,23 @@ package example.lambda
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-class LambdaHandler: RequestHandler<Map<String, Any>, String> {
+class LambdaHandler: RequestHandler<Map<String, Any>, APIGatewayProxyResponseEvent> {
 
   private val json = Json {}
 
-  override fun handleRequest(input: Map<String, Any>, context: Context): String {
-    val message = input["message"]
+  override fun handleRequest(input: Map<String, Any>, context: Context): APIGatewayProxyResponseEvent {
+    val body = json.decodeFromString<Map<String,String>>(input["body"] as String)
+    val message = body["message"]
     context.logger.log("Received message: [$message]")
-    return json.encodeToString(mapOf("response" to "Response to [$message]"))
+    val responseBody = json.encodeToString(mapOf("response" to "Response to [$message]"))
+    return APIGatewayProxyResponseEvent()
+      .withStatusCode(200)
+      .withIsBase64Encoded(false)
+      .withBody(responseBody)
   }
 }
--- stack/src/main/kotlin/example/lambda/Stack.kt
+++ stack/src/main/kotlin/example/lambda/Stack.kt
@@ -4,6 +4,8 @@ import software.amazon.awscdk.core.Construct
 import software.amazon.awscdk.core.Duration
 import software.amazon.awscdk.core.Stack as AwsStack
 import software.amazon.awscdk.core.StackProps
+import software.amazon.awscdk.services.apigateway.LambdaIntegration
+import software.amazon.awscdk.services.apigateway.RestApi
 import software.amazon.awscdk.services.lambda.Code
 import software.amazon.awscdk.services.lambda.Function
 import software.amazon.awscdk.services.lambda.Runtime
@@ -14,12 +16,21 @@ class Stack(
   props: StackProps,
 ): AwsStack(scope, id, props) {
   init {
-    Function.Builder.create(this, "sample-handler")
+    val handler = Function.Builder.create(this, "sample-handler")
       .code(Code.fromAsset("../lambdas/build/libs/lambdas-all.jar"))
       .handler("example.lambda.LambdaHandler")
       .timeout(Duration.seconds(5))
-      .memorySize(1024)
+      .memorySize(512)
       .runtime(Runtime.JAVA_8_CORRETTO)
       .build()
+    val api = RestApi.Builder.create(this, "sample-api")
+      .restApiName("Sample Service")
+      .description("Example api")
+      .build()
+    val templates = mapOf("application/json" to """{ "statusCode": "200" }""")
+    val dealIntegration = LambdaIntegration.Builder.create(handler)
+      .requestTemplates(templates)
+      .build()
+    api.root.addResource("handle").addMethod("POST", dealIntegration)
   }
 }
